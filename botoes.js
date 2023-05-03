c49071edfa1489427cf42ce389629c48bc110ecc
Ana Júlia vieira <av7121217@gmail.com>
2023-05-03 16:51:15 -0300

    .

---
+++ botoes.js
@@ -0,0 +1,32 @@
+function rosa() {
+    const div1 = document.getElementById("div1")
+    
+    const div2 = document.getElementById("div2")
+   
+    const div3 = document.getElementById("div3")
+    
+    const div4 = document.getElementById("div4")
+    
+    div1.style.backgroundColor = "pink";
+    div2.style.backgroundColor = "pink";
+    div3.style.backgroundColor = "pink";
+    div4.style.backgroundColor = "pink";
+     
+}
+
+function roxo() {
+    const div1 = document.getElementById("div1")
+    
+    const div2 = document.getElementById("div2")
+
+    const div3 = document.getElementById("div3")
+    
+    const div4 = document.getElementById("div4")
+
+    div1.style.backgroundColor = "purple";
+    div2.style.backgroundColor = "purple";
+    div3.style.backgroundColor = "purple";
+    div4.style.backgroundColor = "purple";
+
+}
+
