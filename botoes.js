3831d93bb15ac7d7cf776906f3a8b193892b34ea
Ana Júlia vieira <av7121217@gmail.com>
2023-05-04 16:41:34 -0300

    .

--- botoes.js
+++ botoes.js
@@ -1,4 +1,4 @@
-function rosa() {
+function laranja() {
     const div1 = document.getElementById("div1")
     
     const div2 = document.getElementById("div2")
@@ -7,11 +7,10 @@ function rosa() {
     
     const div4 = document.getElementById("div4")
     
-    div1.style.backgroundColor = "pink";
-    div2.style.backgroundColor = "pink";
-    div3.style.backgroundColor = "pink";
-    div4.style.backgroundColor = "pink";
-     
+    div1.style.backgroundColor = "#c75000";
+    div2.style.backgroundColor = "#c75000";
+    div3.style.backgroundColor = "#c75000";
+    div4.style.backgroundColor = "#c75000"; 
 }
 
 function roxo() {
@@ -27,6 +26,34 @@ function roxo() {
     div2.style.backgroundColor = "purple";
     div3.style.backgroundColor = "purple";
     div4.style.backgroundColor = "purple";
-
 }
 
+function marron() {
+    const div1 = document.getElementById("div1")
+    
+    const div2 = document.getElementById("div2")
+
+    const div3 = document.getElementById("div3")
+    
+    const div4 = document.getElementById("div4")
+
+    div2.style.backgroundColor = "#621b00";
+    div3.style.backgroundColor = "#621b00";
+    div1.style.backgroundColor = "#621b00";
+    div4.style.backgroundColor = "#621b00";
+ }
+
+ function azul() {
+    const div1 = document.getElementById("div1")
+    
+    const div2 = document.getElementById("div2")
+
+    const div3 = document.getElementById("div3")
+    
+    const div4 = document.getElementById("div4")
+
+    div1.style.backgroundColor = "blue";
+    div2.style.backgroundColor = "blue";
+    div3.style.backgroundColor = "blue";
+    div4.style.backgroundColor = "blue";
+ }
